f1663a240fc0c3a502f99d745a0767f59cacc27f
Xinan Shan <>
2021-04-29 21:30:42 +0000

    Review 1

--- src/gr11review/part1/Review1.java
+++ src/gr11review/part1/Review1.java
@@ -1,1 +1,34 @@
 package gr11review.part1;
+
+import java.io.*;
+
+public class Review1{
+  public static void main(String[] args){
+    BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+    int Month;
+    int Days;
+    int MonthinDays;
+    int Count;
+    int DaysinTotal
+
+    System.out.println("Enter the month number:");
+    Month = Integer.parseInt(key.readLine());
+
+    System.out.println("Enter the date number:");
+    Days = Integer.parseInt(key.readLine());
+
+    for (Count = 1; Count < Month; intCount++){
+      if (Count == 2){
+        MonthinDays += 28;
+      }else if (Count == 4 || Count == 6 || Count == 9 || Count == 11){
+        MonthinDays += 30;
+      }else{
+        MonthinDays += 31;
+      }
+
+    DaysinTotal = MonthinDays + Days
+    System.out.println("Your days in the year is: " +DaysinTotal+ "days");
+
+    }
+  }
+}
