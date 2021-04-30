8a3d3c7d1939b27112afc42bc8bcfd9c5c3d1798
Xinan Shan <>
2021-04-30 18:38:25 +0000

    review 2

--- src/gr11review/part1/Review2.java
+++ src/gr11review/part1/Review2.java
@@ -7,10 +7,32 @@ public class Review2{
     BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
     int MenuOption;
 
-    System.out.println("0 - print an joke about your hair");
+    System.out.println("0 - print a joke about your hair");
     System.out.println("1 - print a joke about your feet");
     System.out.println("2 - print a joke about your clothes");
     System.out.println("3 - print a joke about your teacher");
-    
+    System.out.println("Choose a menu option: ");
+    MenuOption = Integer.parseInt(key.readLine());
+
+  switch (MenuOption) {
+    case 0:
+      System.out.println("Why do bees have sticky hair?");
+      System.out.println("They always use honeycombs.");
+      break;
+    case 1: 
+      System.out.println("Why do bees have sticky hair?");
+      System.out.println("They always use honeycombs.");
+      break;
+    case 2:
+      System.out.println("Why do bees have sticky hair?");
+      System.out.println("They always use honeycombs.");
+      break;
+    case 3:
+      System.out.println("Why do bees have sticky hair?");
+      System.out.println("They always use honeycombs.");
+      break;
+    default:
+      System.out.println("Invalid menu option");
+    } 
   }
 }
