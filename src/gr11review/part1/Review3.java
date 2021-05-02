d15a8845196a45c994cbd935ed2c7f2d5187dad6
Xinan Shan <>
2021-05-02 19:29:30 +0000

    first loop

--- src/gr11review/part1/Review3.java
+++ src/gr11review/part1/Review3.java
@@ -5,6 +5,18 @@ import java.io.*;
 public class Review3{
   public static void main(String[] args) throws IOException{
     BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+    int Count;
+    int Number = 100;
     
+    for (Count = 20; Count < Number; Count++){
+      if (Count % 2 > 0){
+        System.out.println(Count);
+      }
+    }
+    for (Count = 20; Count < Number; Count++){
+      if (Count % 2 > 0){
+        System.out.println(Count);
+      }
+    }
   }
 }
