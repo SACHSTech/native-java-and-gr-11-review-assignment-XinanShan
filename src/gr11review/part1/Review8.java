4313f871d469322585b37cf645fdaaaf220ff30d
Xinan Shan <>
2021-05-03 01:38:01 +0000

    review 8

--- src/gr11review/part1/Review8.java
+++ src/gr11review/part1/Review8.java
@@ -2,9 +2,23 @@ package gr11review.part1;
 
 import java.io.*;
 
+/**
+*Create a program that simulates 1000 pulls from a slot machine
+* prints out all the outcomes and how many timess a triple was pulled out
+* @author: A. Shan 
+*/
 public class Review8{
   public static void main(String[] args) throws IOException{
     BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
-    
+   
+    //declaring the variables
+    int slot1;
+    int slot2;
+    int slot3;
+    int Count;
+    int Triple = 0;
+
+    // The loop will continue until the count is equal to 1000
+   
   }
 }
