d660e3fe72988752d81b11870569c7f91961e481
Xinan Shan <>
2021-05-03 02:31:07 +0000

    review 7

--- src/gr11review/part1/Review7.java
+++ src/gr11review/part1/Review7.java
@@ -2,9 +2,47 @@ package gr11review.part1;
 
 import java.io.*;
 
+/**
+* Asks the user the input a sentence
+* Count the number of characters, spaces and "a"s in the sentence.
+* Prints a dash for every odd numbered characters in the sentence 
+* @author: A. Shan 
+*/
 public class Review7{
   public static void main(String[] args) throws IOException{
     BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+  //declearing the varaibles
+  String Sentence;
+  int Characters;
+  int Spaces = 0;  
+  int LetterA = 0; 
+  int Count;
+  String Dashes = ""; 
+
+  //Ask the user for the sentence
+  Sentence = key.readLine();
+  //Count the number of letters in the sentence
+  Characters = Sentence.length();
+
+  for (Count = 1; Count <= Characters; Count++){
     
+    / Extracting a single character from the sentence and put it in a char varaible
+    char OneCharacter = Sentence.charAt(Count);
+    
+    // check if the character equal to space or the letter "a" and count them
+    if(OneCharacter == ' '){
+      Spaces++;
+    }else if(OneCharacter == 'a'){
+      LetterA++;
+      }
+    
+    if (Count % 2 != 0){
+      Dashes += "-";
+      }
+    }
+    System.out.println("There are " + Characters + " characters in the sentence");
+    System.out.println("There are " + Spaces + " spaces in the sentence");
+    System.out.println("There are " + LetterA + " letter a in the sentence");
+    System.out.println(Dashes);
   }
 }
